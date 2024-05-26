193a3d728a548dc17d90e2ab3dba4623a0f0321e
Bonioszko <bartek.pers@gmail.com>
2024-05-26 18:58:26 +0200

    first

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,31 +1,55 @@
-import { StyleSheet } from 'react-native';
+import { TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-
+import NfcManager, { NfcTech } from "react-native-nfc-manager";
+NfcManager.start();
 export default function TabTwoScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
-    </View>
-  );
+    const [isScanning, setIsScanning] = useState(false);
+
+    async function readNdef() {
+        setIsScanning(true);
+        try {
+            await NfcManager.requestTechnology(NfcTech.Ndef);
+
+            console.info("NFC tech request successful");
+            const tag = await NfcManager.getTag();
+
+            console.warn("Tag found", tag);
+        } catch (ex) {
+            console.warn("Oops!", ex);
+        } finally {
+            setIsScanning(false);
+            NfcManager.cancelTechnologyRequest();
+        }
+    }
+    return (
+        <View className="flex-1 justify-center items-center bg-blue-800">
+            <Text className="text-2xl font-bold ">Tab Two</Text>
+            <TouchableOpacity onPress={readNdef}>
+                <Text>Scan a Tag</Text>
+            </TouchableOpacity>
+            {isScanning && <Text>Scanning...</Text>}
+            <View lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+            <EditScreenInfo path="app/(tabs)/two.tsx" />
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   title: {
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//   },
+//   separator: {
+//     marginVertical: 30,
+//     height: 1,
+//     width: '80%',
+//   },
+// });
